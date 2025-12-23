98e607ceaef5d01a0be2e06581c3a767c6f0334f
Matt Woodward <mpwoodward@gmail.com>
2025-12-23 11:18:21 -0800

    fix: re-enable SW for PWA install

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
+import { registerSW } from 'virtual:pwa-register'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+
+// Service Worker is REQUIRED for PWA "Install" prompt to appear
+registerSW({ immediate: true })
 
 const app = createApp(App)
 app.use(router)
